4d278df468bdbedb7fb11b55e17bd45598a7f73e
Arve Systad <arve.systad@epinova.no>
2015-01-07 15:27:28 +0100

    Added the editor descriptor from your blog post (renamed it though) - this should be contained in the package.

---
+++ GoogleMapsEditor.cs
@@ -0,0 +1,15 @@
+﻿using EPiServer.Shell.ObjectEditing.EditorDescriptors;
+
+namespace EPiServer.GoogleMapsEditor
+{
+    [EditorDescriptorRegistration(TargetType = typeof(string), UIHint = UIHint)]
+    public class GoogleMapsEditor : EditorDescriptor
+    {
+        public const string UIHint = "CoordinatesEditorDescriptor";
+
+        public GoogleMapsEditor()
+        {
+            ClientEditingClass = "tedgustaf.googlemaps.Editor";
+        }
+    }
+}
